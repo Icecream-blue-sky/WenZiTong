c0aeb4d5052defa7a02e80276ce9fceceff6f30c
Einstein <2871985403@qq.com>
2018-08-07 21:01:47 +0800

    删除WenZiTong-master文件夹

--- WenZiTong-master/app/src/main/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI := armeabi-v7a
-#APP_ABI += armeabi-v7a # you can do either armeabi or armeabi-v7a, steps are the same.
-APP_STL := gnustl_static
-APP_CPPFLAGS := -frtti -fexceptions
-APP_PLATFORM := android-26
